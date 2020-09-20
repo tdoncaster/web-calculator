94f4a4d4986ad1c169a8160c549a416b5d5ebf75
unknown <tim doncaster>
2020-09-20 17:05:55 -0400

    1st set of new code

---
+++ script.js
@@ -0,0 +1,10 @@
+function sum() {
+    var a,b;
+        var a =number(document.getElementById("a").Value)
+        var b =number(document.getElementById("b").Value)
+     sum = a + b 
+        var answer = parseInt(a) + parseInt(b)
+        document.getElementById('answer').innerHTML = answer
+      
+        
+    }
