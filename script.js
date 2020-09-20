0a91da827d7d69df85e576fb7366078d834def5f
unknown <tim doncaster>
2020-09-20 17:58:23 -0400

    2nd set of changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function sum() {
     var a,b;
-        var a =number(document.getElementById("a").Value)
-        var b =number(document.getElementById("b").Value)
+        var a =Number(document.getElementById("a").value)
+        var b =Number(document.getElementById("b").value)
      sum = a + b 
         var answer = parseInt(a) + parseInt(b)
         document.getElementById('answer').innerHTML = answer
