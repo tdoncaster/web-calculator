158b3716ba34559990378b31d1ab78adf946063c
unknown <tim doncaster>
2020-09-27 13:09:59 -0400

    part-two-answer complete

--- script2.js
+++
@@ -1,9 +0,0 @@
-function add(){
-    var a,b,c;
-    a=Number(document.getElementById("first").Value)
-    b=Number(document.getElementById("second").Value)
-    c= a + b;
-    document.getElementById("answer").value= c;
-    }
-        
-    
